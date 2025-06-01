369dc06587bfe75f3c18d5c295efd59e8855b4e8
Josh Edwards <josh@terasoft.com.au>
2025-06-01 14:22:09 +1000

    Generate delauney triangles from terrain points

--- nodes/point.gd
+++ nodes/point.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 class_name Point
 
-var id: int
-var neighbours: Array[int]
+var id:int
+var neighbours:Array[int]
 
 # https://www.reddit.com/r/godot/comments/13pm5o5/instantiating_a_scene_with_constructor_parameters/
 # TODO: Reorganise constructor
--- nodes/web.gd
+++ nodes/web.gd
@@ -1,44 +1,44 @@
 extends Node2D
 
-const WIDTH = 1152
-const HEIGHT = 648
+const CHUNK_WIDTH = 64
+const CHUNK_HEIGHT = 64
 
 @export var noise_scale = 0.1
 @export var point_variation = 0.4
-
-var x_axis_points = 23
-var y_axis_points = 12
-var units_per_x = (WIDTH/x_axis_points)
-var units_per_y = (HEIGHT/y_axis_points)
-@warning_ignore("integer_division")
-var count = x_axis_points*y_axis_points
+@export var view_scale = 10
 
 var noise: FastNoiseLite
 var rng: RandomNumberGenerator
 var web: Array
+var points:Array[Vector2]
 var point_resource: Resource = preload("res://nodes/point.tscn")
 var seed:int = 0
 
 func _init():
 	noise = FastNoiseLite.new()
 	rng = RandomNumberGenerator.new()
-	#noise.noise_type = FastNoiseLite.TYPE_CELLULAR
 	
 	web = []
-	for i in count:
+	for i in CHUNK_WIDTH*CHUNK_HEIGHT:
 		var p = point_resource.instantiate()
 		p.id = i
 		@warning_ignore("integer_division")
-		p.position = Vector2((i%x_axis_points)*units_per_x, (i/x_axis_points)*units_per_y)
+		p.position = Vector2(i%CHUNK_WIDTH, i/CHUNK_HEIGHT)
+		p.position += Vector2(rng.randf()*point_variation, rng.randf()*point_variation)
 		web.append(p)
+		points.append(p.position)
+	
 
 func _draw():
+	var triangle_indices = Geometry2D.triangulate_delaunay(points)
+	#print(triangle_indices)
+	var queue:Array[Vector2] = []
+	for i in triangle_indices.size():
+		queue.append(points[triangle_indices[i]]*view_scale)
+		if i%3 != 2:
+			continue
+		var triangle = PackedVector2Array(queue.duplicate())
+		queue = []
+		draw_colored_polygon(triangle, Color(randf(), randf(), randf()))
 	for point:Node2D in web:
-		@warning_ignore("integer_division")
-		rng.randf()
-		var pos = point.position
-		pos += Vector2(rng.randf()*point_variation*units_per_x, rng.randf()*point_variation*units_per_y)
-		var value = noise.get_noise_2dv(pos*noise_scale)
-		draw_circle(pos, 40, Color(value,0,1-value))
-		# Compare to neighbours and only draw if greatest of neighbours
-		print(value)
+		draw_circle(point.position*view_scale, 2, Color.RED)
