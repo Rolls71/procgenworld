ed096c2c3755910d80d998559427167e6a316d73
Josh Edwards <josh@terasoft.com.au>
2025-06-14 16:44:56 +1000

    Create triangles instead of polygons

--- nodes/terrain_generator.gd
+++ nodes/terrain_generator.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-const CHUNK_WIDTH = 32
-const CHUNK_HEIGHT = 32
+const CHUNK_WIDTH = 8
+const CHUNK_HEIGHT = 8
 const STEP_TIME = 0.01
 const STEP_TIMER = false
 
 @export var point_variance = 0.4
-@export var view_scale = 15
+@export var view_scale = 80
 
 signal terrain_generation_complete
 
@@ -16,6 +16,7 @@ var structure_layer = preload("res://nodes/terrain_generator.tscn")
 @onready var rng:RandomNumberGenerator = RandomNumberGenerator.new()
 @onready var delaunay:Delaunay = Delaunay.new(Rect2(0,0,CHUNK_WIDTH,CHUNK_HEIGHT))
 var tiles:Array[Tile]
+var triangulation:Array[Delaunay.Triangle]
 var posDict = {}
 var start_time
 var last_time
@@ -39,10 +40,11 @@ func _ready():
 			
 	time_check("Instantiated tiles")
 	
-	var triangulation = delaunay.triangulate()
+	triangulation = delaunay.triangulate()
 	time_check("Calculated Delaunay triangulation")
 	
 	var sites:Array[Delaunay.VoronoiSite] = delaunay.make_voronoi(triangulation)
+	
 	time_check("Generated Voronoi sites")
 	
 	## Link voronoi sites and tiles
@@ -57,7 +59,7 @@ func _ready():
 			var site:Delaunay.VoronoiSite = edge.other
 			if not tile.test_adjacency(site.center):
 				tile.neighbours.append(posDict[site.center][0])
-	time_check("Linked sites")
+	time_check("Linked tiles")
 	
 	## Set tiles
 	var remaining = tiles.duplicate()
@@ -78,20 +80,30 @@ func _ready():
 	
 	print("Finished generating terrain in ", (last_time-start_time)/1000.0, " secs")
 	emit_signal("terrain_generation_complete")
+	
 		
 func show_site(tile:Tile):
 	var site = tile.site
-	var polygon = Polygon2D.new()
-	var p = site.polygon
-	p.append(p[0])
-	var temp = []
-	for each in p:
-		temp.append(each*view_scale)
-	polygon.polygon = temp
-	polygon.color = tile.terrain_type.colour
-	polygon.z_index = -1
-	add_child(polygon)
-	
+	var triangles:Array[Delaunay.Triangle] = site.source_triangles
+	for triangle in triangles:
+		if delaunay.is_border_triangle(triangle):
+			continue
+		var polygon = Polygon2D.new()
+		var arr = []
+		arr.append(triangle.a)
+		arr.append(triangle.b)
+		arr.append(triangle.c)
+		arr.append(triangle.a)
+		var packed = PackedVector2Array(arr)
+		
+		var temp = []
+		for each in packed:
+			temp.append(each*view_scale)
+		polygon.polygon = temp
+		polygon.color = tile.terrain_type.colour
+		polygon.z_index = -1
+		add_child(polygon)
+
 func time_check(msg):
 	var t = Time.get_ticks_msec()
 	print(msg," in ", (t-last_time)/1000.0, " secs")
